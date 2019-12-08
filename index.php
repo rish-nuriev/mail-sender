725f64cf67c025e71e2f31c0c94bc4922548d2a9
rish-nuriev <51078267+rish-nuriev@users.noreply.github.com>
2019-12-08 12:08:12 +0200

    Update index.php
    refactor

--- index.php
+++ index.php
@@ -8,5 +8,6 @@ $mailSenderService = new MailSenderService();
 $mailSenderService->addDriver(new PHPMailSender(), 'mail');
 $mailSenderService->addDriver(new SwiftMailSender(), 'swift');
 $mailSenderService->setDriver('swift');
+$mailSenderService->send('nuriev_rishat@mail.ru', 'Hello', 'This email has been sent by Swift');
 $mailSenderService->setDriver('mail');
-$mailSenderService->send('nuriev_rishat@mail.ru', 'Hello', 'This email sent by Swift');
+$mailSenderService->send('nuriev_rishat@mail.ru', 'Hello', 'This email has been sent by Mail function');
